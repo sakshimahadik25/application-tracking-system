d88939cb01419cb5049e1ba7fc1372d3d3257066
mtkumar123 <89176219+mtkumar123@users.noreply.github.com>
2021-11-21 08:57:28 -0500

    Change host string for new mongodb

--- backend/app.py
+++ backend/app.py
@@ -134,7 +134,7 @@ with open('application.yml') as f:
     password = info['password']
     app.config['MONGODB_SETTINGS'] = {
         'db': 'appTracker',
-        'host': f'mongodb+srv://{username}:{password}@apptracker.goffn.mongodb.net/appTracker?retryWrites=true&w=majority'
+        'host': f'mongodb+srv://{username}:{password}@applicationtracker.287am.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
     }
 db = MongoEngine()
 db.init_app(app)
