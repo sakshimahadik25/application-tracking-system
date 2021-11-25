f4a17253f0e04df4f3112964aadec4ab93793e8c
Ashwin Das <ashwindasr@gmail.com>
2021-11-25 00:19:44 -0500

    Fix shutdown.sh

--- shutdown.sh
+++ shutdown.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-kill $(ps -a | grep npm | awk '{print $1}')
-kill $(ps -a | grep node | awk '{print $1}')
-kill $(ps -A | grep python | awk '{print $1}')
+kill $(ps | grep npm | awk '{print $1}')
+kill $(ps | grep node | awk '{print $1}')
+kill $(ps | grep flask | awk '{print $1}')
