12e9cbda84f41d9961940bfe054373accc382b33
Sakshi Mahadik <sakshimahadik2511@gmail.com>
2023-10-16 16:26:31 -0400

    Profile page icon added to nav

--- frontend/src/sidebar/Sidebar.js
+++ frontend/src/sidebar/Sidebar.js
@@ -20,6 +20,9 @@ export default class Sidebar extends Component {
           <div onClick={() => this.props.switchPage('ManageResumePage')}>
             <i className="fas fa-folder left-nav-icon"></i>
           </div>
+          <div onClick={() => this.props.switchPage('ProfilePage')}>
+            <i className="fas fa-user left-nav-icon"></i>
+          </div>
         </div>
       </div>
     )
